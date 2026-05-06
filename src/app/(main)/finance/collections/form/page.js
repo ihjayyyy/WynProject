099a0716f1ead1b8e21955ad2c59b8ebd78127a3
mark-xavier-diaz <103088678+mark-xavier-diaz@users.noreply.github.com>
2026-05-07 05:55:11 +0800

    wrap suspense collections

--- src/app/(main)/finance/collections/form/page.js
+++ src/app/(main)/finance/collections/form/page.js
@@ -1,6 +1,13 @@
-'use client'
-import CollectionForm from '@/components/Collection/CollectionForm';
+'use client';
+import { Suspense } from 'react';
+import dynamic from 'next/dynamic';
+
+const CollectionForm = dynamic(() => import("@/components/Collection/CollectionForm"), { ssr: false });
 
 export default function Page() {
-  return (<CollectionForm />);
+  return (
+    <Suspense fallback={"Loading..."}>
+      <CollectionForm/>
+    </Suspense>
+  );
 }
--- src/app/(main)/finance/collections/page.js
+++ src/app/(main)/finance/collections/page.js
@@ -1,6 +1,13 @@
-'use client'
-import CollectionLanding from '@/components/Collection/CollectionLanding';
+'use client';
+import { Suspense } from 'react';
+import dynamic from 'next/dynamic';
+
+const CollectionLanding = dynamic(() => import("@/components/Collection/CollectionLanding"), { ssr: false });
 
 export default function Page() {
-  return (<CollectionLanding />);
+  return (
+    <Suspense fallback={"Loading..."}>
+      <CollectionLanding/>
+    </Suspense>
+  );
 }
