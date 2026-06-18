61d41df48be93f722223f63e81218e860d5663e4
Ian Quinto <codedianq@gmail.com>
2026-06-18 23:25:48 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  env: {
-    NEXT_PUBLIC_API_URL: 'https://odrtechapi.app/ims/api',
-  },
+
   images: {
     unoptimized: true,
   },
